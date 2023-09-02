30eed8c350d105c67721ca4378e8a9418b6e09a8
mdjasimuddim <mdjasimuddimruet@gmail.com>
2023-09-02 22:59:54 +0600

    all files are added

---
+++ ninja-gaming/src/routes/about.svelte
@@ -0,0 +1,22 @@
+<div class="about">
+    <h2>About us</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+        repellendus, quod iusto modi excepturi maxime est error adipisci eos
+        quis deleniti, nihil illo cupiditate quasi nam impedit molestiae
+        delectus. Minima est odio corporis velit dolor repudiandae nesciunt iure
+        autem, exercitationem minus placeat fuga repellendus temporibus alias
+        nobis sint perspiciatis? Ducimus aut magni iste laboriosam quia,
+        molestiae eius explicabo architecto saepe?
+    </p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        display: block;
+        text-align: center;
+        margin: 20px auto;
+    }
+</style>
--- ninja-gaming/src/routes/index.svelte
+++ ninja-gaming/src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="index">
+    <h2>Welcome</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga rerum
+        earum exercitationem provident libero perspiciatis blanditiis illum
+        dolores deserunt minima reiciendis odio consequatur, voluptatibus
+        eligendi asperiores praesentium ipsam explicabo natus voluptatem neque
+        temporibus ipsa repellendus repellat reprehenderit. Provident nihil
+        minima esse earum inventore, facilis exercitationem animi consectetur
+        magni ut ea. Earum provident ex necessitatibus possimus ipsam accusamus
+        incidunt, quaerat nesciunt?
+    </p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        display: block;
+        text-align: center;
+        margin: 20px auto;
+    }
+</style>
